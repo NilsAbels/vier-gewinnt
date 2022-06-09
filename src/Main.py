4957ea41ad37105f32eb129d81bdec9dfba2bde3
Nils <nils.abels23@gmail.com>
2022-06-09 20:43:17 +0200

    commit

--- src/Main.py
+++ src/Main.py
@@ -1,19 +1,18 @@
 import numpy
 
 def Spielzug(spielfeld,spieler,spalte):
-    print(spielfeld)
-    print(spieler)
+
     if spielfeld[0][spalte] != 0:
         print("zug nicht möglich")
     for i in range(0, len(spielfeld)):
         if i == len(spielfeld) - 1:
             spielfeld[i][spalte] = spieler
-            if CheckGewinnervert(spieler, spielfeld, spalte, i) or CheckGewinnerhorizontal(spieler,spielfeld,i):
+            if CheckGewinnervert(spieler, spielfeld, spalte, i) or CheckGewinnerhorizontal(spieler,spielfeld,i) or  CheckGewinnerDiagonal(spieler,spielfeld,i,spalte):
                 print("gewonnen")
             break
         elif spielfeld[i+1][spalte] != 0:
             spielfeld[i][spalte] = spieler
-            if CheckGewinnervert(spieler, spielfeld, spalte, i) or CheckGewinnerhorizontal(spieler,spielfeld,i):
+            if CheckGewinnervert(spieler, spielfeld, spalte, i) or CheckGewinnerhorizontal(spieler,spielfeld,i) or CheckGewinnerDiagonal(spieler,spielfeld,i,spalte):
                 print("gewonnen")
             break
 
@@ -21,10 +20,26 @@ def Spielzug(spielfeld,spieler,spalte):
 
 
 def CheckGewinnervert(spieler, spielfeld, spalte, zeile):
+
+    zaehler = 0
+
+    for s in range(0,len(spielfeld)):
+        if spielfeld[zeile][s] == spieler:
+            zaehler += 1
+        else:
+            zaehler = 0
+        if zaehler == 4:
+            return True
+    return False
+
+
+
+
+
     activeRow = True
     rowcount = 0
-    while(activeRow):   #vertikale Überprüfung
-        if(zeile == len(spielfeld)):    #reihe außerhalb des Spielfelds
+    while(activeRow):
+        if(zeile == len(spielfeld)):
             return False
         if spielfeld[zeile][spalte] == spieler:
             rowcount += 1
@@ -52,22 +67,70 @@ def CheckGewinnerhorizontal(spieler, spielfeld, zeile):
 
 
 
-def CheckGewinnerhorizontal(spieler,spielfeld,zeile,spalte):
+def CheckGewinnerDiagonal(spieler,spielfeld,zeile,spalte):
+     zaehler = 0
+     startZ = zeile
+     startS = spalte
+     while(startS>0 and startZ>0):
+         startZ -= 1
+         startS -= 1
+
+     while(startS<len(spielfeld[zeile]) and startZ<len(spielfeld)):
+         if spielfeld[startZ][startS] == spieler:
+             zaehler += 1
+         else:
+             zaehler = 0
+         if zaehler == 4:
+            return True
+         startZ += 1
+         startS += 1
+
+     zaehler = 0
+     startZ = zeile
+     startS = spalte
+     while (startS > 0 and startZ < len(spielfeld) - 1 ):
+         startZ += 1
+         startS -= 1
+
+     while (startS < len(spielfeld[zeile]) and startZ  > 0 ):
+         if spielfeld[startZ][startS] == spieler:
+             zaehler += 1
+         else:
+             zaehler = 0
+         if zaehler == 4:
+             return True
+         startZ -= 1
+         startS += 1
+
+
+     return False
+
 
 
 
 
 spielfeld = numpy.zeros((8,8))  #spieldfeld[zeile][spalte]
+print(spielfeld)
 spieler = 1
 while(True):
     eingabe = input("Spalte: ")
     if eingabe == "stop":
         break
-    Spielzug(spielfeld,spieler,int(eingabe)-1)
-    if spieler == 1:
-        spieler = 2
+    if Spielzug(spielfeld,spieler,int(eingabe)-1):
+        print(spieler , "hat gewonnen")
+        eingabe = input("wollt ihr nochmal spielen? (Y/N)")
+        if eingabe == "Y":
+            spielfeld = numpy.zeros(8,8)
+        else:
+            exit()
+
+
     else:
-        spieler = 1
+
+      if spieler == 1:
+        spieler = 2
+      else:
+         spieler = 1
 
 
 
